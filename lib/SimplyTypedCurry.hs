41d893a33f7fde11800024d266ba9cfc905b86fc
marcodemayda <marcodemayda@gmail.com>
2025-04-26 18:12:53 +0200

    left comment of what is missing in Abs inference case

--- lib/SimplyTypedCurry.hs
+++ lib/SimplyTypedCurry.hs
@@ -54,9 +54,9 @@ checkInferVAR (ga, (la, t)) = any (\(x,y) -> (T(V x),y) == (la,t)) ga
 
 checkInferABS :: Judgment -> Judgment -> Bool
 checkInferABS (ga1, (la1,t1)) (ga2, (la2,t2)) = case t2 of
-    To si ta ->    True
+    To si ta ->    True -- missing "ga1 contains (x, si) for some variable x, and ga2 == ga1-(x,si)"
                 && t1 == si && t2 == ta
-                && la2 == T (L (V 1))
+                && la2 == T (L 1 (preTer la1))
     _ -> False
     
 
