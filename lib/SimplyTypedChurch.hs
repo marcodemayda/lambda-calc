b4094cc3d6ab08e7694c422e0c552a91b7ef0e58
marcodemayda <marcodemayda@gmail.com>
2025-04-28 23:37:33 +0200

    main functions of SimplyTypedChurch done

--- lib/SimplyTypedChurch.hs
+++ lib/SimplyTypedChurch.hs
@@ -17,9 +17,10 @@ import Data.List
 ---BEGINNINGS-----------
 -- 
 
-type RawVar = Var 
-data RawTerm = R RawVar | RA RawTerm RawTerm | RL (RawVar, Type) RawTerm
-
+type RawVar = Var
+data RawTerm = R RawVar | RA RawTerm RawTerm | RL (RawVar, ArType) RawTerm
+    deriving (Eq,Show)
+--NOTE: to do, gotta tranlate all the work for terms to RawTerms to get alpha equivalence.
 
 freeRawVars :: RawTerm -> [Var]
 freeRawVars (R x) = [x]
@@ -29,8 +30,53 @@ freeRawVars (RL (x,_) m) = freeRawVars m \\ [x]
 
 
 substRaw :: RawTerm -> Var -> RawTerm -> RawTerm
-substRaw (R x) y n 
+substRaw (R x) y n
     | x == y = n
     | otherwise = R x
 substRaw (RA p q) y n = RA (substRaw p y n) (substRaw q y n)
-substRaw (RL x m) y n = undefined
+substRaw (RL y m) x n
+    | x /= fst y && notElem x (freeRawVars n) = RL y (substRaw m x n)
+    | otherwise = RL y m
+
+
+
+-------------------------- Judgmetns -----------------------
+
+type JudgmentCh = (Context, (RawTerm, ArType))
+
+inferChurchVAR :: JudgmentCh -> Bool
+inferChurchVAR (ga, (ra, t)) = any (\(x,y) -> (R x,y) == (ra,t)) ga
+
+inferChurchABS :: JudgmentCh -> JudgmentCh -> Bool
+inferChurchABS (ga1, (la1,t1)) (ga2, (la2,t2)) = case la2 of
+    (RL x m) ->  case t2 of
+        si `To` ta -> snd x == si
+                            && t1 == ta
+                            && ga2 == ga1 \\ [(fst x, si)]
+                            && la1 == m
+        _ -> False
+    _ -> False
+
+
+inferChurchAPP :: JudgmentCh -> JudgmentCh -> JudgmentCh ->  Bool
+inferChurchAPP (ga1, (la1, t1)) (ga2, (la2,t2)) (ga3, (la3, t3)) =
+    case t1 of
+        ta `To` si -> ga1 == ga2 && ga2 == ga3
+                    && la3 ==  RA la1 la2
+                    && t2 == si && t3 == ta
+        _ -> False
+
+
+
+
+
+---- OTHER FUNCTIONS --------
+
+churchToPre :: RawTerm -> LambdaPreTerm
+churchToPre (R x) = (V x)
+churchToPre (RA m n) = A (churchToPre m) (churchToPre n)
+churchToPre (RL x m) = L (fst x) (churchToPre m)
+
+
+churchToCurry :: RawTerm -> LambdaTerm
+churchToCurry = T . churchToPre
--- lib/SimplyTypedCurry.hs
+++ lib/SimplyTypedCurry.hs
@@ -19,7 +19,7 @@ import Data.List
 
 type Atom = Integer
 
-data Type = Ty Atom | To Type Type
+data ArType = Ty Atom | To ArType ArType
     deriving (Eq, Show)
 
 typeAtoms :: [Atom]
@@ -28,9 +28,9 @@ typeAtoms = [1..]
 -- types = map Ty typeAtoms ++ (map To (map Ty typeAtoms))
 
 -- A context is a "set" of tuples asigning variables to a type.
-type Context = [(Var, Type)]
+type Context = [(Var, ArType)]
 
-atomsOf :: Type -> [Atom]
+atomsOf :: ArType -> [Atom]
 atomsOf (Ty x) = [x]
 atomsOf (x `To` y) = atomsOf x ++ atomsOf y
 
@@ -39,12 +39,12 @@ contextVar = map fst
 contextDom :: Context -> [Var]
 contextDom = contextVar
 
-contextTypes :: Context -> [Type]
+contextTypes :: Context -> [ArType]
 contextTypes = map snd
-contextRan :: Context -> [Type]
+contextRan :: Context -> [ArType]
 contextRan = contextTypes
 
-contextAdd :: Context -> (Var,Type) -> Context
+contextAdd :: Context -> (Var,ArType) -> Context
 contextAdd = undefined
 
 -- domain contexts need to be (partial) functional w.r.t variables.
@@ -52,34 +52,34 @@ checkWellDefContext :: Context -> Bool
 checkWellDefContext ga = map fst ga == nub (map fst ga)
 
 -- type of a variable in a context.
-typeOf :: Var -> Context -> Maybe Type
+typeOf :: Var -> Context -> Maybe ArType
 typeOf = lookup
 
 
 -- THe list of variables in a context that have a of a given type
-typedVars ::  Context -> Type  -> [Var]
+typedVars ::  Context -> ArType  -> [Var]
 typedVars g t
     | t `elem` contextRan g = [x | x <- contextDom g, (x,t) `elem` g]
     | otherwise = []
 
 
-contextExtend :: Context -> (Var,Type) -> Maybe Context
+
+contextExtend :: Context -> (Var,ArType) -> Maybe Context
 contextExtend ga tup
     |checkWellDefContext (ga ++ [tup]) = Just $ ga ++ [tup]
     | otherwise = Nothing
 
 
 
-
 ------------ JUDGMENTS --------------
-type Judgment = (Context, (LambdaTerm, Type))
+type JudgmentCu = (Context, (LambdaTerm, ArType))
 
 
 
-checkInferVAR :: Judgment -> Bool
+checkInferVAR :: JudgmentCu -> Bool
 checkInferVAR (ga, (la, t)) = any (\(x,y) -> (T (V x),y) == (la,t)) ga
 
-checkInferABS :: Judgment -> Judgment -> Bool
+checkInferABS :: JudgmentCu -> JudgmentCu -> Bool
 checkInferABS (ga1, (la1,t1)) (ga2, (la2,t2)) = case la2 of
         T (L x m) -> case t2 of
                 si `To` ta -> typeOf x ga1 == Just si
@@ -89,7 +89,7 @@ checkInferABS (ga1, (la1,t1)) (ga2, (la2,t2)) = case la2 of
                 _ -> False
         _ -> False
 
-checkInferAPP :: Judgment -> Judgment -> Judgment ->  Bool
+checkInferAPP :: JudgmentCu -> JudgmentCu -> JudgmentCu ->  Bool
 checkInferAPP (ga1, (la1, t1)) (ga2, (la2,t2)) (ga3, (la3, t3)) =
     case t1 of
         ta `To` si -> ga1 == ga2 && ga2 == ga3
