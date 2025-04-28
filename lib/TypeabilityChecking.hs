7374eb06067740db3a8a383ddb56ab5c44ab22d8
marcodemayda <marcodemayda@gmail.com>
2025-04-28 23:39:14 +0200

    fixed some name changes. Things compile, and there's a good bulk, so using branches from now

--- lib/TypeabilityChecking.hs
+++ lib/TypeabilityChecking.hs
@@ -15,10 +15,10 @@ import SimplyTypedCurry
 
 ------------- THE MAIN PROBLEMS -----------------------
 -- Type check problem: decide if a judgment holds (i.e. is derivable, i.e., i think, there is a prooftree to it, whose leaves are all VAR, and each child respects ABS or APP)
-checkTypeCheck :: Judgment -> Bool
+checkTypeCheck :: JudgmentCu -> Bool
 checkTypeCheck (ga, (m,ta)) = undefined
 
-checkSimplyTypeCheck :: Judgment -> Bool
+checkSimplyTypeCheck :: JudgmentCu -> Bool
 checkSimplyTypeCheck = undefined
 
 -- Type Typability/Reconstruction Problem: decide if for a given term, there exists a context and type Gamma,sigma, such that  Gamma M sigma type-checks
@@ -35,7 +35,7 @@ typableConstruction  m = undefined
 -- Variable not in scope: m
 
 -- Type inhabitation/emptiness problem: given a Type, decide wether there is (closed) LambdaTerm such that Judment with the empty context holds for them.
-checkInhabitable :: Type -> Bool
+checkInhabitable :: ArType -> Bool
 checkInhabitable = undefined
 
 
@@ -43,7 +43,7 @@ checkInhabitable = undefined
 
 ----------------- WORKINGS ---------------------
 
-auxiliarVars :: LambdaPreTerm -> Type -> [Var]
+auxiliarVars :: LambdaPreTerm -> ArType -> [Var]
 auxiliarVars (V x) (Ty p)
     | x == p = [x]
     | otherwise = []
@@ -57,12 +57,12 @@ auxiliarVars (L _ m) (ta `To` si) = undefined
 
 
 -- NOTE: both are unfinished; naive w.r.t to variables in the application case. Also need to make the renaming so that each Em is unique for a term m.
-equationSysteM :: LambdaTerm -> [Type]
+equationSysteM :: LambdaTerm -> [ArType]
 equationSysteM (T (V _)) = []
 equationSysteM (T (A p q)) = equationSysteM (T p) ++ equationSysteM (T q) ++ [typeSysteM (T q) `To` typeSysteM (T (A p q))]
 equationSysteM (T (L x m)) = equationSysteM $ T (substPreTot m x (V (freshPreVar m)))
 
-typeSysteM :: LambdaTerm -> Type
+typeSysteM :: LambdaTerm -> ArType
 typeSysteM (T (V x)) = Ty x
 typeSysteM (T (A m n)) = Ty (freshPreVar (A m n))
 typeSysteM (T (L x m)) = Ty (freshPreVar m) `To` typeSysteM (T $ substPreTot m x (V (freshPreVar m)))
